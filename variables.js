66c8c45533e1c825353cad9eaf4b3999857a6015
Anders Gustavsson <gustavsson09@hotmail.com>
2017-10-03 21:14:00 +0200

    Added more variables in variable.js. Console.log them to see values in webbrowser

--- variables.js
+++ variables.js
@@ -1,3 +1,22 @@
 // Training javascript from Udemy and git terminal
 
 var name = "Krang";
+
+personObject = {
+	name: "Anders",
+	age: 32,
+	status: "single"
+}
+console.log(personObject.name, personObject.age, personObject.status)
+
+carsArray = ['Mustang','Dodge Charger','El Camino'];
+
+for(var i = 0; i < carsArray.length; i++){
+	console.log(carsArray[i]);
+}
+
+ var name = "Anders"
+ var number = 32;
+ var floatNumber = 32.8;
+
+ console.log(name, number, floatNumber);
