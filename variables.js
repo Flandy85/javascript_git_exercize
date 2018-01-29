276cbc6c0a7d49c5f7156e625da23ab6a7af9161
Anders Gustavsson <gustavsson09@hotmail.com>
2018-01-29 15:15:11 +0100

    Added input tag and commented it out in index.html, created queryselector in variables.js for testing manupilating the DOM

--- variables.js
+++ variables.js
@@ -49,11 +49,11 @@ console.log(url);
 var playStr = "Practicing javascript variables and methods to get at better understanding";
 console.log(playStr);
 
-function myFunction() {
-    var playStr = "Practicing javascript variables and methods to get at better understanding",
-    	result = playStr.
-    document.getElementById("title").innerHTML = result;
-}
+// function myFunction() {
+//     var playStr = "Practicing javascript variables and methods to get at better understanding",
+//     	result = playStr.
+//     document.getElementById("title").innerHTML = result;
+// }
 // Project1, making a url variable practice 
 var array1 = ["https://wastelandsolution.se"], // array1
 	array2 = ["/wordpress/kontakta-oss/"], // array2
@@ -97,17 +97,43 @@ else {
 */
 
 // Prompt for see user input
-var firstName = prompt('What is your name?');
-// Check if the name is null, clicking cancel button
-if(firstName === null) {
-	alert("You don't have a name!");
-}
-//Check if user puts in a name when prompted
-else if (firstName) {
-	alert("Hello " + firstName);
-}
+// var firstName = prompt('What is your name?');
+// // Check if the name is null, clicking cancel button
+// console.log(firstName);
+// if(firstName === null) {
+// 	alert("You don't have a name!");
+// 	console.log(firstName);
+// }
+// //Check if user puts in a name when prompted
+// else if (firstName == "Anders") {
+// 	alert("Hello " + firstName);
+// 	console.log(firstName);
+// }
 // If empty and press ok button
-else {
-	alert("Hello guest");
-}
+// else {
+// 	alert("Hello guest");
+// 	console.log(firstName);
+// }
+// select id main and change content with querySelector.
+// var main = document.querySelector('#main');
+// main.innerHTML = "Hello World";
+// main.innerHTML += ' I am editing this page with javascript! ';
+// content = main.innerHTML;
+// main.innerHTML += 'Edited';
+// main.innerHTML = 'Please REMOVE me!';
+// content = content.toUpperCase().replace(' ', '-');
+// main.innerHTML = content;
+// // Select inputfield class textfield
+// var txt = document.querySelector('.textfield');
+// txt.innerHTML = 'TEST'; // Value test
+// txt.value = 'A new attribute value'; // Change value to new text
 
+// input = txt.value; // input = txt variable value
+// alert(input);	// alert input variable
+// console.log(input);
+// data = txt.value;
+// console.log(data);
+var main = document.querySelector('#main');
+main.innerHTML = 'Hello World';
+main.style.border = '1px solid red';
+main.style.paddingTop = '50px';
